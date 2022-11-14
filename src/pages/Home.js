0b6c98235d2a32f407d372f2df00f4edec49ee53
cherish553 <42311932+cherish553@users.noreply.github.com>
2022-11-14 18:16:13 +0800

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "antd";
+import "antd/dist/antd.css";
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <Button>111</Button>
+    </div>
+  );
+}
